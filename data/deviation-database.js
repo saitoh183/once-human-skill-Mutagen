3ce7f0e996fc60bc31dcb925856b42acc9f3d7b5
saitoh183 <saitoh183@gmail.com>
2026-06-07 22:22:52 -0400

    Add deviation and skill search tab

---
+++ data/deviation-database.js
@@ -0,0 +1,765 @@
+'use strict';
+
+window.DEVIATION_SKILL_DATABASE = {
+  "deviations": [
+    {
+      "name": "Atomic Lighter",
+      "image": "https://ohdex.gg/assets/012_ATOMIC-LIGHTER.png",
+      "skills": [
+        "Flame Bullet",
+        "Rite of Strength",
+        "Elemental Overload",
+        "Energy Conduit",
+        "Speed Up",
+        "Group Elemental Overload",
+        "Flame Impact",
+        "Incendiary Bullet"
+      ]
+    },
+    {
+      "name": "Atomic Snail",
+      "image": "https://ohdex.gg/assets/034_ATOMIC-SNAIL.png",
+      "skills": [
+        "Cryo Bullet",
+        "Energy Conduit",
+        "Deviant Energy Conduit",
+        "Elemental Overload",
+        "Lightning Pierce",
+        "Crystal Ambush"
+      ]
+    },
+    {
+      "name": "Butterfly Emmissary",
+      "image": "https://ohdex.gg/assets/005_BUTTERFLY'S-EMISSARY.png",
+      "skills": [
+        "Savage Strike",
+        "Dodge",
+        "Resonant Skybound"
+      ]
+    },
+    {
+      "name": "Buzzy Bee",
+      "image": "https://ohdex.gg/assets/001_BUZZY-BEE.png",
+      "skills": [
+        "Psi Bullet",
+        "Rapid Charge",
+        "Healing Bullet",
+        "Healing Aura",
+        "Cyclone"
+      ]
+    },
+    {
+      "name": "By-the-Wind",
+      "image": "https://ohdex.gg/assets/026_BY-THE-WIND.png",
+      "skills": [
+        "Psi Bullet",
+        "Psi Ray",
+        "Group Physical Enhancement",
+        "Cyclone",
+        "Heavy Pressure",
+        "Penetrating Charge"
+      ]
+    },
+    {
+      "name": "Chaos Extradimensional Cat",
+      "image": "https://ohdex.gg/assets/039_EXTRADIMENSIONAL-CAT.png",
+      "skills": [
+        "Charging Aura",
+        "Sweep",
+        "Kitty Punch",
+        "Rapid Charge",
+        "Heavy Pressure",
+        "Charge"
+      ]
+    },
+    {
+      "name": "Chaos Pyro Dino",
+      "image": "https://ohdex.gg/assets/047_PYRO-DINO.png",
+      "skills": [
+        "Flaming Flight",
+        "Burn Aura",
+        "Speed Up",
+        "Flame Jet",
+        "Incendiary Bullet",
+        "Meteor"
+      ]
+    },
+    {
+      "name": "Chaos Snows Sprite",
+      "image": "https://ohdex.gg/assets/052_WINTER-SPIRIT.png",
+      "skills": [
+        "Elemental Overload",
+        "Cryo Bullet",
+        "Ice Crystal Barrage",
+        "Frost Satellite",
+        "Speed Up",
+        "Ice Crystal Ambush"
+      ]
+    },
+    {
+      "name": "Chaos Mr Wish",
+      "image": "https://ohdex.gg/assets/020_MR.-WISH.png",
+      "skills": [
+        "Space Twister",
+        "Weakspot Sniper",
+        "Resonant Skybound",
+        "Rapid Fire",
+        "Fate Coin",
+        "Self-Recharge"
+      ]
+    },
+    {
+      "name": "Digby Boy",
+      "image": "https://ohdex.gg/assets/024_DIGBY-BOY.png",
+      "skills": [
+        "Savage Strike",
+        "Provoke",
+        "Chrysalis",
+        "Heavy Pressure",
+        "Charge",
+        "Shield"
+      ]
+    },
+    {
+      "name": "Disco Ball",
+      "image": "https://ohdex.gg/assets/003_DISCO-BALL.png",
+      "skills": [
+        "Psi Bullet",
+        "Resonant Skybound",
+        "Energy Conduit",
+        "Weakspot Sniper",
+        "Healing Link",
+        "Healing Bullet"
+      ]
+    },
+    {
+      "name": "Dr-Teddy",
+      "image": "https://ohdex.gg/assets/016_DR.-TEDDY.png",
+      "skills": [
+        "Savage Strike",
+        "Rapid Charge",
+        "Healing",
+        "Healing Bullet",
+        "Charge",
+        "Healing Aura"
+      ]
+    },
+    {
+      "name": "Dreamcatcher",
+      "image": "https://ohdex.gg/assets/008_DREAMCATCHER.png",
+      "skills": [
+        "Cyclone",
+        "Dodge",
+        "Rapid Charge",
+        "Resonant Skybound",
+        "Mesh Traction",
+        "Space Twister"
+      ]
+    },
+    {
+      "name": "Electric Eel",
+      "image": "https://ohdex.gg/assets/010_ELECTRIC-EEL.png",
+      "skills": [
+        "Electric Bullet",
+        "Lightning",
+        "Rapid Charge",
+        "Lightning Pierce",
+        "Elemental Overload",
+        "Electrical Blast"
+      ]
+    },
+    {
+      "name": "Enchanting Void",
+      "image": "https://ohdex.gg/assets/021_ENCHANTING-VOID.png",
+      "skills": [
+        "Dodge",
+        "Space Twister",
+        "Resist Death",
+        "Psi Bullet",
+        "Intimidation"
+      ]
+    },
+    {
+      "name": "Festering Gel",
+      "image": "https://ohdex.gg/assets/006_FESTERING-GEL.png",
+      "skills": [
+        "Psi Bullet",
+        "Healing Bullet",
+        "Healing Aura",
+        "Chrysalis",
+        "Heavy Pressure"
+      ]
+    },
+    {
+      "name": "Fetch-lot-Bunny",
+      "image": "https://ohdex.gg/assets/015_FETCH-A-LOT-BUNNY.png",
+      "skills": [
+        "Savage Strike",
+        "Provoke",
+        "Healing",
+        "Rush",
+        "Resist Death",
+        "Charge",
+        "Chrysalis"
+      ]
+    },
+    {
+      "name": "Flame Essence",
+      "image": "https://ohdex.gg/assets/036_FLAME-ESSENCE.png",
+      "skills": [
+        "Flame Bullet",
+        "Burn Aura",
+        "Self-Recharge",
+        "Dodge",
+        "Flame Impact",
+        "Incendiary Bullet"
+      ]
+    },
+    {
+      "name": "Frog the Leaper",
+      "image": "https://ohdex.gg/assets/004_FROG-THE-LEAPER.png",
+      "skills": [
+        "Savage Strike",
+        "Provoke",
+        "Chrysalis",
+        "Charge",
+        "Whirlwind Slash",
+        "Heavy Pressure"
+      ]
+    },
+    {
+      "name": "Gazocchio",
+      "image": "https://ohdex.gg/assets/051_NIGHT-WATCH-LAMP.png",
+      "skills": [
+        "Psi Bullet",
+        "Physical Enhancement",
+        "Physical Shockwave",
+        "Psi Ray"
+      ]
+    },
+    {
+      "name": "Gingerbread House",
+      "image": "https://ohdex.gg/assets/019_GINGERBREAD-HOUSE.png",
+      "skills": [
+        "Psi Bullet",
+        "Rapid Charge",
+        "Dodge",
+        "Chrysalis",
+        "Cyclone",
+        "Black Cat's Gaze",
+        "Corrosion Aura"
+      ]
+    },
+    {
+      "name": "Growthshroom",
+      "image": "https://ohdex.gg/assets/031_GROWSHROOM.png",
+      "skills": [
+        "Savage Strike",
+        "Provoke",
+        "Chrysalis",
+        "Shield",
+        "Whirlwind Slash",
+        "Heavy Pressure",
+        "Resist Death"
+      ]
+    },
+    {
+      "name": "Grumpy Bulb",
+      "image": "https://ohdex.gg/assets/029_GRUMPY-BULB.png",
+      "skills": [
+        "Psi Bullet",
+        "Rush",
+        "Space Twister",
+        "Charge",
+        "Wild Shot",
+        "Resist Death",
+        "Chrysalis"
+      ]
+    },
+    {
+      "name": "Hydronaut",
+      "image": "https://ohdex.gg/assets/054_HYDRONAUT-FISH.png",
+      "skills": [
+        "Electric Bullet",
+        "Electrical Blast",
+        "Lightning Pierce",
+        "Self-Recharge",
+        "Lucky Light",
+        "Frost Vortex",
+        "Elemental Overload"
+      ]
+    },
+    {
+      "name": "H37",
+      "image": "https://ohdex.gg/assets/018_H37.png",
+      "skills": [
+        "Psi Bullet",
+        "Psi Ray",
+        "Shield",
+        "Chrysalis",
+        "Charge",
+        "Emergency Shield"
+      ]
+    },
+    {
+      "name": "Ice Pot",
+      "image": "https://ohdex.gg/assets/057_ICE-BOTTLE.png",
+      "skills": [
+        "Cryo Bullet",
+        "Provoke",
+        "Resist Death",
+        "Ice Crystal Ambush",
+        "Damage Link",
+        "Self-Healing"
+      ]
+    },
+    {
+      "name": "Invincible Sun",
+      "image": "https://ohdex.gg/assets/056_ETERNAL-SUN.png",
+      "skills": [
+        "Meteor",
+        "Flame Bullet",
+        "Elemental Overload",
+        "Charging Aura",
+        "Flame Impact"
+      ]
+    },
+    {
+      "name": "Lethal Rabbit",
+      "image": "https://ohdex.gg/assets/042_LETHAL-RABBIT.png",
+      "skills": [
+        "Savage Strike",
+        "Blink",
+        "Blade Declaration",
+        "Sweep",
+        "Speed Up",
+        "Blink Strike"
+      ]
+    },
+    {
+      "name": "Logging Beaver",
+      "image": "https://ohdex.gg/assets/023_LOGGING-BEAVER.png",
+      "skills": [
+        "Savage Strike",
+        "Chrysalis",
+        "Sweep",
+        "Penetrating Charge",
+        "Blink Strike",
+        "Beaver Combo"
+      ]
+    },
+    {
+      "name": "Lonewolf Whisper",
+      "image": "https://ohdex.gg/assets/013_CANINE-SKULL.png",
+      "skills": [
+        "Savage Strike",
+        "Blink Strike",
+        "Physical Enhancement",
+        "Speed Up",
+        "Penetrating Charge",
+        "Sweep"
+      ]
+    },
+    {
+      "name": "Masonic Pyramid",
+      "image": "https://ohdex.gg/assets/027_MASONIC-PYRAMID.png",
+      "skills": [
+        "Electric Bullet",
+        "Speed Up",
+        "Lightning",
+        "Sugarcoated Shell",
+        "Torpedo",
+        "Pulse Orb",
+        "Group Elemental Overload",
+        "Electrical Blast"
+      ]
+    },
+    {
+      "name": "Mini Feaster",
+      "image": "https://ohdex.gg/assets/040_MINI-FEASTER.png",
+      "skills": [
+        "Psi Bullet",
+        "Outer God’s Tentacle",
+        "Space Twister",
+        "Speed Up",
+        "Healing",
+        "Physical Enhancement"
+      ]
+    },
+    {
+      "name": "Mini Wonder",
+      "image": "https://ohdex.gg/assets/038_MINI-WONDER.png",
+      "skills": [
+        "Lightning",
+        "Pulse Orb",
+        "Elemental Overload",
+        "Blackhole Jet",
+        "Space Twister"
+      ]
+    },
+    {
+      "name": "Mr Wish",
+      "image": "https://ohdex.gg/assets/020_MR.-WISH.png",
+      "skills": [
+        "Space Twister",
+        "Weakspot Sniper",
+        "Resonant Skybound",
+        "Rapid Fire",
+        "Fate Coin",
+        "Self-Recharge"
+      ]
+    },
+    {
+      "name": "Nutcracker",
+      "image": "https://ohdex.gg/assets/022_NUTCRACKER.png",
+      "skills": [
+        "Rapid Fire",
+        "Wild Shot",
+        "Chrysalis",
+        "Resist Death",
+        "Speed Up",
+        "Weakspot Sniper"
+      ]
+    },
+    {
+      "name": "Orb Lightning",
+      "image": "https://ohdex.gg/assets/043_ORB-LIGHTNING.png",
+      "skills": [
+        "Electric Bullet",
+        "Lightning",
+        "Pulse Orb",
+        "Electrical Blast",
+        "Resonant Skybound"
+      ]
+    },
+    {
+      "name": "Paper Doll",
+      "image": "https://ohdex.gg/assets/041_PAPER-DOLL.png",
+      "skills": [
+        "Flame Bullet",
+        "Crystal Ambush",
+        "Pulse Orb",
+        "Lightning",
+        "Cryo Bullet",
+        "Flame Impact",
+        "Physical Shockwave"
+      ]
+    },
+    {
+      "name": "Party Monkey",
+      "image": "https://ohdex.gg/assets/035_PARTY-MONKEY.png",
+      "skills": [
+        "Cyclone",
+        "Flame Bullet",
+        "Provoke",
+        "Flame Twister",
+        "Speed Up",
+        "Rapid Charge"
+      ]
+    },
+    {
+      "name": "Polar Jelly",
+      "image": "https://ohdex.gg/assets/009_POLAR-JELLY.png",
+      "skills": [
+        "Cryo Bullet",
+        "Frost Satellite",
+        "Crystal Ambush",
+        "Group Elemental Overload",
+        "Emergency Shield",
+        "Frost Vortex"
+      ]
+    },
+    {
+      "name": "Pup Buddy",
+      "image": "https://ohdex.gg/assets/002_PUP-BUDDY.png",
+      "skills": [
+        "Savage Strike",
+        "Rite of Strength",
+        "Super Flammable",
+        "Charge",
+        "Whirlwind Slash",
+        "Explosive"
+      ]
+    },
+    {
+      "name": "Pyro Dino",
+      "image": "https://ohdex.gg/assets/047_PYRO-DINO.png",
+      "skills": [
+        "Flaming Flight",
+        "Burn Aura",
+        "Speed Up",
+        "Flame Jet",
+        "Incendiary Bullet",
+        "Meteor"
+      ]
+    },
+    {
+      "name": "Rainman",
+      "image": "https://ohdex.gg/assets/033_RAIN-MAN.png",
+      "skills": [
+        "Cryo Bullet",
+        "Chrysalis",
+        "Resist Death",
+        "Rainy Peril"
+      ]
+    },
+    {
+      "name": "Rebecca",
+      "image": "https://ohdex.gg/assets/025_REBECCA.png",
+      "skills": [
+        "Healing Bullet",
+        "Energy Conduit",
+        "Charging Aura",
+        "Group Elemental Overload",
+        "Group Physical Enhancement",
+        "Agni’s Emissary"
+      ]
+    },
+    {
+      "name": "Shattered Maiden",
+      "image": "https://ohdex.gg/assets/037_SHATTERED-MAIDEN.png",
+      "skills": [
+        "Psi Bullet",
+        "Psi Ray",
+        "Rapid Charge",
+        "Rite of Strength",
+        "Resist Death",
+        "Mesh Traction"
+      ]
+    },
+    {
+      "name": "Snow Globe",
+      "image": "https://ohdex.gg/assets/030_SNOW-GLOBE.png",
+      "skills": [
+        "Cryo Bullet",
+        "Frost Satellite",
+        "Rapid Charge",
+        "Ice Crystal Ambush",
+        "Lightning Pierce",
+        "Elemental Overload"
+      ]
+    },
+    {
+      "name": "Space Turner",
+      "image": "https://ohdex.gg/assets/014_SPACE-TURNER.png",
+      "skills": [
+        "Psi Bullet",
+        "Energy Conduit",
+        "Resonant Skybound",
+        "Mesh Traction",
+        "Psi Ray",
+        "Space Twister"
+      ]
+    },
+    {
+      "name": "Strange Door",
+      "image": "https://ohdex.gg/assets/048_EXTRADIMENSIONAL-DOOR.png",
+      "skills": [
+        "Chrysalis",
+        "Pulse Ray",
+        "Provoke",
+        "Pulse Orb",
+        "Void Zone",
+        "Lightning Pierce"
+      ]
+    },
+    {
+      "name": "Voodoo Doll",
+      "image": "https://ohdex.gg/assets/028_VOODOO-DOLL.png",
+      "skills": [
+        "Psi Bullet",
+        "Mesh Traction",
+        "Speed Up",
+        "Resist Death",
+        "Corrosion Aura"
+      ]
+    },
+    {
+      "name": "Zeno Purifier",
+      "image": "https://ohdex.gg/assets/032_ZENO-PURIFIER.png",
+      "skills": [
+        "Blink",
+        "Cyclone",
+        "Dodge",
+        "Sweep",
+        "Whirlwind Slash",
+        "Shadow Strike"
+      ]
+    },
+    {
+      "name": "Zapmander",
+      "image": "https://www.oncehumandb.com/_next/image?url=https%3A%2F%2Fdxouo2ijk4yg2kj7.public.blob.vercel-storage.com%2Fimages%2Fentities%2F99000059.png&w=256&q=75",
+      "skills": [
+        "Electric Bullet",
+        "Lightning",
+        "Pulse Orb",
+        "Electrical Blast",
+        "Elemental Overload",
+        "Chain Lightning"
+      ]
+    },
+    {
+      "name": "Whalepup",
+      "image": "https://www.oncehumandb.com/_next/image?url=https%3A%2F%2Fdxouo2ijk4yg2kj7.public.blob.vercel-storage.com%2Fimages%2Fentities%2F99000064.png&w=256&q=75",
+      "skills": [
+        "Cryo Bullet",
+        "Shield",
+        "Frost Vortex",
+        "Resist Death",
+        "Ice Crystal Ambush",
+        "Whale Invasion"
+      ]
+    },
+    {
+      "name": "Director Fox",
+      "image": "https://www.oncehumandb.com/_next/image?url=https%3A%2F%2Fdxouo2ijk4yg2kj7.public.blob.vercel-storage.com%2Fimages%2Fentities%2F99000070.png&w=256&q=75",
+      "skills": [
+        "Psi Bullet",
+        "Healing Bullet",
+        "Healing Aura",
+        "Healing Link",
+        "Group Physical Enhancement",
+        "Animal Expert"
+      ]
+    },
+    {
+      "name": "Soul Summoner",
+      "image": "https://www.oncehumandb.com/_next/image?url=https%3A%2F%2Fdxouo2ijk4yg2kj7.public.blob.vercel-storage.com%2Fimages%2Fentities%2F99000066.png&w=256&q=75",
+      "skills": [
+        "Cryo Bullet",
+        "Elemental Overload",
+        "Frost Vortex",
+        "Rain Arrow",
+        "Dodge",
+        "Arrow Storm"
+      ]
+    },
+    {
+      "name": "Tar Pudding",
+      "image": "https://ohdex.gg/assets/053_TAR-PUDDING.png",
+      "skills": [
+        "Flame Bullet",
+        "Elemental Overload",
+        "Incendiary Bullet",
+        "Burn Aura",
+        "Meteor",
+        "Crude Oil Spill"
+      ]
+    },
+    {
+      "name": "Hug-in-a-Bowl",
+      "image": "https://ohdex.gg/assets/011_HUG-IN-A-BOWL.png",
+      "skills": [
+        "Psi Bullet",
+        "Chrysalis",
+        "Heavy Pressure",
+        "Resonant Skybound",
+        "Rush",
+        "Self-Recharge"
+      ]
+    },
+    {
+      "name": "Upper World Spawn",
+      "image": "https://ohdex.gg/assets/044_UPPER-WORLD-SPAWN.png",
+      "skills": [
+        "Flame Bullet",
+        "Meteor",
+        "Pulse Ray",
+        "Burn Aura",
+        "Provoke",
+        "Upper World Protection"
+      ]
+    }
+  ],
+  "shops": {
+    "cat": [
+      "Savage Strike",
+      "Psi Bullet",
+      "Electric Bullet",
+      "Chrysalis",
+      "Physical Enhancement",
+      "Shield",
+      "Sweep",
+      "Rush",
+      "Heavy Pressure",
+      "Charging Aura",
+      "Psi Ray",
+      "Blink Strike",
+      "Blast",
+      "Rite of Strength",
+      "Self-Recharge",
+      "Group Elemental Overload",
+      "Group Physical Enhancement",
+      "Chain Lightning",
+      "Kitty Punch"
+    ],
+    "dino": [
+      "Healing Bullet",
+      "Flame Bullet",
+      "Whirlwind Slash",
+      "Lightning Pierce",
+      "Penetration Charge",
+      "Incendiary Bullet",
+      "Pulse Orb",
+      "Flame Jet",
+      "Speed Up",
+      "Burn Aura",
+      "Intimidation",
+      "Meteor",
+      "Pulse Ray",
+      "Frost Vortex",
+      "Corrosion Aura",
+      "Rain Arrow",
+      "BBQ",
+      "Flaming Flight",
+      "Animal Expert",
+      "Crude Oil Spill"
+    ],
+    "snow": [
+      "Cryo Bullet",
+      "Self-Healing",
+      "Elemental Overload",
+      "Electrical Blast",
+      "Wild Shot",
+      "Healing Link",
+      "Cyclone",
+      "Mesh Traction",
+      "Flame Impact",
+      "Emergency Shield",
+      "Ice Crystal Ambush",
+      "Frost Satellite",
+      "Ice Crystal Barrage",
+      "Beaver Combo",
+      "Shadow Strike",
+      "Outer God’s Tentacle",
+      "Blade Declaration",
+      "Super Flammable",
+      "Blackhole Jet",
+      "Agni’s Emissary",
+      "Arrow Storm"
+    ],
+    "wish": [
+      "Lightning",
+      "Rapid Fire",
+      "Weakspot Sniper",
+      "Resist Death",
+      "Healing Aura",
+      "Deviant Energy Conduit",
+      "Dodge",
+      "Provoke",
+      "Blink",
+      "Damage Link",
+      "Resonant Skybound",
+      "Space Twister",
+      "Fate Coin",
+      "Void Zone",
+      "Torpedo",
+      "Black Cat's Gaze",
+      "Sugarcoated Shell",
+      "Rainy Peril",
+      "Lucky Light",
+      "Whale Invasion",
+      "Upper World Protection"
+    ]
+  }
+};
